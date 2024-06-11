903337de731248d2c418b4dc539194f2514a16a1
yrautcnasdemo <yrautcnas@msn.com>
2024-06-11 16:24:09 +0200

    add backoffice

--- src/index.php
+++ src/index.php
@@ -4,19 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/fonts.css">
     <title>Take a FeaRIEND</title>
 </head>
 <body>
+    <section class="intro">
     <header class="index-header">
-        <img src="/img/logosite.png" height="170px" alt="">
-        <nav class="main-nav">
-            <li><a href="">Accueil</a></li>
-            <li><a href="">Catégories</a></li>
-            <li><a href="">A propos</a></li>
+        <img src="/img/logosite.png" height="70px" alt="">
+        <nav >
+            <ul class="main-nav">
+                <li><a href="">Accueil</a></li>
+                <li><a href="">Catégories</a></li>
+                <li><a href="">A propos</a></li>
+            </ul>
         </nav>
-        <div>
-            <button>Connexion/S'inscrire</button>
+        <div class="header-btn">
+            <button class="btnlog"><a class="btnloga" href="inscription.php">Connexion S'inscrire</a></button>
         </div>
     </header>
+
+    
+        <article class="text-intro">
+            <div>
+            <h1 class="title">Take a FeaRIEND</h1>
+            <p class="text-content">Bienvenue sur <span class="sub-title">Take a FeaRIEND</span> 
+            Votre boutique en ligne pour adopter des Créatures aussi étonnantes qu’amicales.</p>
+            <p class="text-content2">*Nous déclinons toutes reponsabilitées en cas de mort brutale</p>
+            </div>
+        </article>
+    </section>
+
+
 </body>
 </html>
