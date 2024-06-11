c40068281a703520a16a6fdb274100259357cfd4
yrautcnasdemo <yrautcnas@msn.com>
2024-06-11 11:13:28 +0200

    inscription page back-end complet

--- src/inscription.php
+++ src/inscription.php
@@ -1,14 +1,15 @@
 <?php
     //On vérifie si le formulaire a était envoyé 
     if(!empty($_POST)){
-        if(isset($_POST["last_name"], $_POST["email"], $_POST["pass"])
-        && !empty($_POST["last_name"]) && !empty($_POST["email"]) && !empty($_POST["pass"])){
+        if(isset($_POST["nom"], $_POST["prenom"], $_POST["email"], $_POST["pass"], $_POST["pass2"])
+        && !empty($_POST["nom"]) && !empty($_POST["prenom"]) && !empty($_POST["email"]) && !empty($_POST["pass"]) && !empty($_POST["pass2"])){
 
             //formulaire complet
-            $pseudo = strip_tags($_POST["nickname"]);
+            $nom = strip_tags($_POST["nom"]);
+            $prenom = strip_tags($_POST["prenom"]);
 
             if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){ 
-              //filter_var et FILTER_VALIDATE_EMAIL serve a sécuriser l'adresse mail en back-end
+              //filter_var et FILTER_VALIDATE_EMAIL servent a sécuriser l'adresse mail en back-end
                 die("L'adresse eMail est incorrecte");
             }
 
@@ -38,18 +39,19 @@
                 //On hash le mdp
                 $pass = password_hash($_POST["pass"], PASSWORD_ARGON2ID);
                 // Mot de passe confirmé, procédez à la logique d'inscription (par exemple, hachage et stockage du mot de passe)
-                    echo "Mot de passe confirmé, inscription en cours...";
+                    header("Location: index.php");
                     } else {
                         die("Les mots de passe ne correspondent pas.");
                     }
                 }
 
             //on écrit la requête sql d'inscription
-            $sql = "INSERT INTO users(username, email, pass) VALUES (:pseudo, :email, '$pass')";
+            $sql = "INSERT INTO users(nom, prenom, email, pass, roles) VALUES (:nom, :prenom, :email, '$pass', 'user')";
 
             $requete = $db->prepare($sql);
 
-            $requete->bindValue(":pseudo", $pseudo, PDO::PARAM_STR);
+            $requete->bindValue(":nom", $nom, PDO::PARAM_STR);
+            $requete->bindValue(":prenom", $prenom, PDO::PARAM_STR);
             $requete->bindValue("email", $_POST["email"], PDO::PARAM_STR);
 
             $requete->execute();
@@ -60,7 +62,6 @@
             }
     } 
 
-    require_once("connect.php");
 ?>
 
 
@@ -72,28 +73,30 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/fonts.css">
 </head>
 <body>
-    <h1>Take a FeaRIEND</h1>
-    <form class="p-4 p-md-5 border rounded-3 bg-body-tertiary center-form">
+    <section class="full-box">
+    <h1 class="font-take">Take a FeaRIEND</h1>
+    <form method="POST" class="p-4 p-md-5 border rounded-3 bg-body-tertiary center-form">
         <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="floatingInput" placeholder="Alice">
+                <input type="text" class="form-control" name="nom" id="floatingInput" placeholder="Alice">
                 <label for="floatingInput">Nom</label>
         </div>
         <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="floatingInput" placeholder="Blue">
+                <input type="text" class="form-control" name="prenom" id="floatingInput" placeholder="Blue">
                 <label for="floatingInput">Prénom</label>
         </div>
         <div class="form-floating mb-3">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">Email address</label>
+                <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com">
+                <label for="email">Email address</label>
         </div>
         <div class="form-floating mb-3">
-                <input type="password" class="form-control" id="pass" placeholder="Password">
+                <input type="password" class="form-control" name="pass" id="pass" placeholder="Password">
                 <label for="pass">Password</label>
         </div>
         <div class="form-floating mb-3">
-                <input type="password" class="form-control" id="pass2" placeholder="Confirme Password">
+                <input type="password" class="form-control" name="pass2" id="pass2" placeholder="Confirme Password">
                 <label for="pass2">Confirme Password</label>
         </div>
         <div class="checkbox mb-3">
@@ -108,6 +111,7 @@
 
 
     </div>
+    </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
