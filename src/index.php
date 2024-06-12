5a203f1fda5e8c62c07046247a932a33ea55ff73
yrautcnasdemo <yrautcnas@msn.com>
2024-06-12 13:57:00 +0200

    synchro

--- src/index.php
+++ src/index.php
@@ -12,7 +12,29 @@
 
 <body>
     <section class="intro">
-        <header class="index-header">
+        <header>
+        <div class="logo">
+            <img src="./img/logosite.png" alt="Logo Take a FeaRIEND">
+        </div>
+        <nav id="nav">
+            <ul>
+                <div class="container-li">
+                    <li><a href="#">Accueil</a></li>
+                    <li><a href="#">Catégories</a></li>
+                    <li><a href="#">A propos</a></li>
+                </div>
+                <div class="container-button-li">
+                    <li><button class="Login">Login</button></li>
+                </div>
+            </ul>
+            <div id="burger">
+                <img src="./img/burger-bar.png" alt="Menu Burger">
+            </div>
+        </nav>
+    </header>
+
+
+        <!-- <header class="index-header">
             <img src="/img/logosite.png" height="70px" alt="">
             <nav>
                 <ul class="main-nav">
@@ -24,7 +46,7 @@
             <div class="header-btn">
                 <button class="btnlog"><a class="btnloga" href="inscription.php">Connexion S'inscrire</a></button>
             </div>
-        </header>
+        </header> -->
 
 
         <article class="text-intro">
