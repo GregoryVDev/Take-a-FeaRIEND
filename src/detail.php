53b3a0feeece85c0b8101be89f8fdb9c76b02470
yrautcnasdemo <yrautcnas@msn.com>
2024-06-14 14:05:31 +0200

    add detail.php front & back and updat index.php back-end

--- src/detail.php
+++ src/detail.php
@@ -1,33 +1,59 @@
+<?php 
+    session_start();
+    require_once("connect.php");
+    require_once("./template/header.php");
+
+    if(!isset($_GET['id'])){
+        die('Id pas trouvé');
+    }
+
+    $id = $_GET['id'];
+
+    $sql = "SELECT * FROM animaux WHERE id = :id";
+    $requete = $db->prepare($sql);
+    $requete->bindValue(':id', $id, PDO::PARAM_INT);
+    $requete->execute();
+
+    $animal = $requete->fetch(PDO::FETCH_ASSOC);
+
+    if(!$animal){
+        die("animal pas trouvé");
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet" href="style.css">
-    <title>Détailles de ma recherche</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="./css/produits/produits.css">
+    <link rel="stylesheet" href="./css/produits/produits-responsive.css">
+    <link rel="stylesheet" href="./css/fonts.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Détails de <?= htmlspecialchars($animal["name"], ENT_QUOTES, 'UTF-8') ?></title>
 </head>
-<body>
+
+<body class="detail-body">
     <main class="container">
         <div class="row">
             <section class="col-12">
 
-                <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+                <div class="bg-dark row flex-lg-row-reverse align-items-center g-5 pb-5 detail-card">
                     <div class="col-10 col-sm-8 col-lg-6">
-                        <img src="/img/nointernet.jpg" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="auto" height="auto" loading="lazy">
+                        <img src="/img/dangereux/105813.png" class="d-block mx-lg-auto" alt="Bootstrap Themes" width="auto" height="600px" loading="lazy">
                     </div>
                     <div class="col-lg-6">
-                    <h1>Entreprise : zfzfazfzafzaf</h1>
-                                    <p>ID : 1</p>
-                                    <p>Statut : zafzfzafz</p>
-                                    <p>Date postulation : zafzafazf</p>
-                                    <p>Date relance : zafzafazfazf</p>
-                                    <p>Type postulation : azfzfafzaf</p>
-                                    <p>Méthode postulation : afzafzafzafzaf</p>
-                                    <p>Poste : azfzafzafzf</p>
-                                    <p>Type contrat : azfzafzafzaaz</p>
-                                    <p>Adresse mail : azfazfzafzaf</p>
-                                    <p>commentaire : zafzafzafafzafaffa</p>
+                            <h1 class="detail-name"><?= $animal['name'] ?></h1>
+                            <p class="detail-id">ID: <?= $animal['id'] ?></p>
+                            <p class="detail-category">Catégorie: <?= $animal['category'] ?></p>
+                            <p class="detail-description">Description: </p>
+                            <p class="detail-description"><?= $animal['content'] ?></p>
+                            <p class="detail-price">Prix: <?= $animal['price'] ?>€</p>
+                            <button type="button" class="btn btn-outline-warning btn-buy">+ Panier</button>
                         <div class="d-grid gap-2 d-md-flex justify-content-md-start">
                         </div>
                     </div>
@@ -35,6 +61,6 @@
             </section>
         </div>
     </main>
-    
-</body>
-</html>
+
+    <?php
+    require_once("./template/footer.php");
