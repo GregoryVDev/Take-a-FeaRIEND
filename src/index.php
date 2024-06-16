c507238b391dfbf535740c70e0c1536918af936c
GregoryVDev <arexo@hotmail.fr>
2024-06-16 17:37:55 +0200

    commit conflics

--- src/index.php
+++ src/index.php
@@ -10,7 +10,6 @@ $requete->execute();
 
 $animals = $requete->fetchAll(PDO::FETCH_ASSOC);
 
-// var_dump($animals);
 ?>
 
 
@@ -30,38 +29,6 @@ $animals = $requete->fetchAll(PDO::FETCH_ASSOC);
 
 <body>
     <section class="intro">
-        <header>
-            <div class="logo">
-                <img src="./img/logosite.png" alt="Logo Take a FeaRIEND">
-            </div>
-            <nav id="nav">
-                <ul>
-                    <div class="container-li">
-                        <li><a href="#">Accueil</a></li>
-                        <li><a href="#">Catégories</a></li>
-                        <li><a href="#">A propos</a></li>
-                    </div>
-                    <div class="container-button-li">
-
-                        <?php if (empty($_SESSION['user'])) { ?>
-                            <div class="container-button-li">
-                                <button type="button" class="Login"><a class="login-link" href="ConnexionUser.php">Login</a></button>
-                            </div>
-                        <?php } else { ?>
-                            <div class="container-button-li">
-                                <button type="button" class="Logout"><a class="logout-link" href="DeconnexionUser.php">Logout</a></button>
-                            </div>
-                        <?php } ?>
-
-
-                    </div>
-                </ul>
-                <div id="burger">
-                    <img src="./img/burger-bar.png" alt="Menu Burger">
-                </div>
-            </nav>
-        </header>
-
 
         <?php require_once("./template/header.php"); ?>
 
